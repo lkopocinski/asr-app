e8f1cbab467eae3e0fd08e1af122a3b2352f9d65
Łukasz Kopociński <lkopocinski@gmail.com>
2024-06-20 23:18:18 +0200

    Show results as a table, select output directory

--- src/asr_app/app.py
+++ src/asr_app/app.py
@@ -3,10 +3,26 @@ from tkinter import Tk, filedialog
 from typing import Union
 
 import gradio as gr
+import pandas as pd
 import whisper
 
 import settings as st
 from progress import ProgressListener, create_progress_listener_handle
+
+
+def on_browse_dirs():
+    root = Tk()
+    root.attributes("-topmost", True)
+    root.focus_force()
+    root.withdraw()
+
+    dir_path = filedialog.askdirectory(
+        initialdir=st.INITIAL_BROWSE_DIRS,
+    )
+
+    root.destroy()
+
+    return dir_path
 
 
 def on_browse_files():
@@ -28,8 +44,10 @@ def on_browse_files():
     return file_paths
 
 
-def on_transcribe(files_paths: str, model_name: str, progress=gr.Progress()):
+def on_transcribe(files_paths: str, output_dir: str, model_name: str, progress=gr.Progress()):
+    progress(progress=0.33, desc=f"Loading model: {model_name}")
     model = whisper.load_model(model_name)
+    progress(progress=1, desc=f"Model {model_name} loaded.")
 
     class FileProgressListener(ProgressListener):
         def __init__(self, file_path: str):
@@ -42,16 +60,25 @@ def on_transcribe(files_paths: str, model_name: str, progress=gr.Progress()):
         def on_finished(self):
             gr.Info(message=self.finished_message)
 
+    results = []
+
     files_paths = files_paths.split('\n')
     for path in files_paths:
         track_listener = FileProgressListener(file_path=path)
         with create_progress_listener_handle(track_listener):
             result = model.transcribe(str(path), verbose=False)
 
-        save_path = Path(path).with_suffix(".txt")
-        save_path.write_text(result["text"])
+        if Path(output_dir).is_dir():
+            save_path = Path(output_dir) / Path(path).name
+            save_path = save_path.with_suffix(".txt")
+        else:
+            save_path = Path(path).with_suffix(".txt")
 
-    return "Zakończono"
+        save_path.write_text(result["text"])
+        results.append((str(path), str(save_path)))
+
+    df = pd.DataFrame(results, columns=['Plik audio', 'Plik tekstowy'])
+    return df
 
 
 with gr.Blocks(title=st.TITLE) as demo:
@@ -71,6 +98,18 @@ with gr.Blocks(title=st.TITLE) as demo:
                     show_progress="hidden",
                 )
 
+            with gr.Accordion(label="Folder do zapisu:", open=True):
+                output_dir = gr.Markdown()
+                browse_button_dir = gr.Button(
+                    value="Wybierz folder",
+                    variant="secondary",
+                )
+                browse_button_dir.click(
+                    on_browse_dirs,
+                    outputs=output_dir,
+                    show_progress="hidden",
+                )
+
             model_dropdown = gr.Dropdown(
                 label="Model",
                 choices=['base', 'small', 'medium', 'large'],
@@ -85,12 +124,15 @@ with gr.Blocks(title=st.TITLE) as demo:
 
         with gr.Column(scale=5):
             header = gr.Markdown("""# Wyniki""")
-            output_values = gr.Markdown()
+            output_values = gr.DataFrame(
+                headers=['Plik audio', 'Plik tekstowy'],
+                col_count=(2, "fixed"),
+            )
 
     transcribe_button.click(
         on_transcribe,
-        inputs=[input_paths, model_dropdown],
-        outputs=[output_values],
+        inputs=[input_paths, output_dir, model_dropdown],
+        outputs=output_values,
     )
 
 if __name__ == '__main__':
--- src/asr_app/settings.py
+++ src/asr_app/settings.py
@@ -1,3 +1,4 @@
 INITIAL_BROWSE_FILE = '/home/lukas/Downloads'
+INITIAL_BROWSE_DIRS = '/home/lukas/'
 
 TITLE = "Audio transcription"
